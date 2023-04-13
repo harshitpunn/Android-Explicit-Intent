07f521b34c321a9c188c6a2b50cc5aee3f45e736
Harshit Pun <harshitpunn@gmail.com>
2023-04-12 17:50:38 -0700

    Initial commit

---
+++ app/src/main/java/com/example/sharingdatainexplicitintents/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.sharingdatainexplicitintents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private Button submitBtn;
+    private EditText nameEditText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        submitBtn = findViewById(R.id.submitBtn);
+        nameEditText = findViewById(R.id.nameEditText);
+
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               /*Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                Bundle b = new Bundle();
+                b.putString("name", nameEditText.getText().toString());
+                intent.putExtras(b);
+                startActivity(intent);*/
+                //passDataFirstMethod();
+                passDataSecondMethod();
+            }
+        });
+
+    }
+
+    private void passDataFirstMethod () {
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        Bundle b = new Bundle();
+        b.putString("name", nameEditText.getText().toString());
+        intent.putExtras(b);
+        startActivity(intent);
+    }
+
+    private void passDataSecondMethod () {
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        intent.putExtra("name", nameEditText.getText().toString());
+        startActivity(intent);
+    }
+
+
+}
